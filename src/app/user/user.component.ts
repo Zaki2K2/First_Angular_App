a2f3d379b3d9d9d80337a5f7108a7f53515c6f6a
Zaki Shah <Zakis2k2@gmail.com>
2024-11-09 21:33:33 +0500

    Update UserComponent, Task in Tasks Component  and related files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, Output, EventEmitter, output } from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
+import { type User } from './user.model'
 
+
+
+// const USERS: User[] = DUMMY_USERS;
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
 
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+  // @Input({ required: true }) selected!: boolean;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  // select = output<string>();
+
+
+
+  get imagePath()
+  {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+onSelectUser()  {
+  this.select.emit(this.user.id);
+ }
 }
